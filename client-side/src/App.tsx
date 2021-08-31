9768fa5eae2353552025099ccc4c81c98db46e81
TRAN <p.n.tran999@gmail.com>
2021-08-31 23:34:20 +0700

    Fetch data from API and display data on client side

---
+++ client-side/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import { Header, Icon, List } from 'semantic-ui-react';
+
+function App() {
+  const [activities, setActivities] = useState([]);
+  useEffect(()=>{
+    axios.get("http://localhost:5000/api/activities").then(response=>{
+      console.log(response);
+      setActivities(response.data);
+    })
+  },[]);
+
+  return (
+    <div>
+        <Header as="h2" content="Activities" icon="users"></Header> 
+        <List>
+         {activities.map((act:any)=>(
+           <List.Item key={act.id}>
+             {act.title}
+             </List.Item>
+         ))}
+       </List>
+    </div>
+  );
+}
+
+export default App;
